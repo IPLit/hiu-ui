c2df8eed3d4bc3b1e74bea7bf8a9119044a15bb4
shilpa <sparab@iplit.in>
2025-08-26 18:00:31 +0530

    Removing UTC from moment date

--- src/components/common/HealthInfo/FhirResourcesUtils.js
+++ src/components/common/HealthInfo/FhirResourcesUtils.js
@@ -156,7 +156,7 @@ const formatDateString = function (aDate, includeTime) {
   if (aDate) {
     const dateString = aDate.toString();
     if (dateString.length > 0) {
-      const momentDate = moment(aDate);
+      const momentDate = moment.tz(aDate, 'DD/MM/YYYY hh:mm A', 'Asia/Kolkata');
       let dt = momentDate.utcOffset(Config.TIMEZONE_OFFSET, true).format('DD/MM/YYYY');
       if (includeTime) {
         dt = momentDate.utcOffset(Config.TIMEZONE_OFFSET, true).format('DD/MM/YYYY hh:mm A');
